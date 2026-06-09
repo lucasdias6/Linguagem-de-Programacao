f866ccf737309b908a7d8ef9022aeb6a0eeefe50
lucasdias6 <4555323113@estudante.sed.sc.gov.br>
2026-06-09 13:38:51 -0300

    exercíco importar função

---
+++ 8-Pacotes/1-Exercicios/Exercicio-4/main.py
@@ -0,0 +1,12 @@
+# Exercício 4: O Formulário de Login do Sistema (Nível: Fácil)
+# Objetivo: Compreender como enviar uma informação coletada por input no arquivo principal para uma função simples de validação que está guardada em um módulo local.
+# Enunciado:
+# Crie um arquivo chamado seguranca.py. Dentro dele, crie uma função chamada validar_login(nome, senha). Se o nome digitado for igual a "admin", e a senha for igual a “123Testar”  a função deve retornar a frase: "Acesso Liberado!". Caso contrário, deve retornar: "Acesso negado!".
+# Crie um segundo arquivo chamado main.py na mesma pasta. Importe a função validar_usuario. No programa principal, use um input para perguntar o nome do usuário, passe esse nome para a função importada e exiba a resposta na tela.
+
+from seguranca import validar_login
+
+nome = input("Usuário: ")
+senha = input("Senha: ")
+
+validar_login(nome, senha)
